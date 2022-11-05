2e0bfc6cc5d0f3602f22ebf520f1c6c386da0579
jkookyz <sitisalwaazizah01@gmail.com>
2022-11-05 11:07:42 +0700

    MODUL1_SALWA

---
+++ MODUL1/src/User.java
@@ -0,0 +1,13 @@
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    public String Nama;
+    public int No.Handphone;
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+    public void register(){
+        System.out.println("Register sukses");
+        System.out.println("Nama : " + Nama);
+        System.out.println("No. Handphone : " + No.Handphone);
+    }
+}
